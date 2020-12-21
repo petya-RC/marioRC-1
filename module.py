1473a2cf82fbd80923119e373397aa1c31e700c8
petya <74210929+petya-RC@users.noreply.github.com>
2020-12-21 19:51:01 +0200

    v.0.
    я исправил вроде теперь должно запустица

--- module.py
+++ module.py
@@ -1,3 +1,14 @@
-def gribchek(xMin, xMax, yMin, yMax, x, y, statusB):
+import os
+import pygame
+
+
+def print_text (text, x, y, color, win, size, font_type='D:\GAME_MARIO\Pixel_Sans_Serif.ttf'):
+    font_type = pygame.font.Font(font_type, size)
+    text = font_type.render(text, True, color)
+    win.blit(text, (x, y))
+
+def gribchek(xMin, xMax, yMin, yMax, x, y, statusB, score):
     if x >= xMin and x <= xMax and y <= yMin and y >= yMax:
         statusB = True
+        score += 100
+    
