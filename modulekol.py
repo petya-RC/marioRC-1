5bfe7b2c4e417c2e86b6430205f3341b237119bf
petya <74210929+petya-RC@users.noreply.github.com>
2020-12-25 17:06:29 +0200

    Add files via upload

---
+++ modulekol.py
@@ -0,0 +1,309 @@
+class colaider ():
+    xMin = 0
+    xMax = 0
+    yMin = 0
+    yMax = 0
+
+
+trumpet1 = colaider()
+trumpet1.xMin = 865
+trumpet1.xMax = 960
+trumpet1.yMax = 290
+trumpet1.yMin = 365
+
+trumpet2 = colaider()
+trumpet2.xMin = 1190
+trumpet2.xMax = 1275
+trumpet2.yMax = 265
+trumpet2.yMin = 365
+
+trumpet3 = colaider()
+trumpet3.xMin = 1448
+trumpet3.xMax = 1535
+trumpet3.yMax = 235
+trumpet3.yMin = 365
+
+trumpet4 = colaider()
+trumpet4.xMin = 1800
+trumpet4.xMax = 1890
+trumpet4.yMax = 235
+trumpet4.yMin = 365
+
+trumpet5 = colaider()
+trumpet5.xMin = 5195
+trumpet5.xMax = 5276
+trumpet5.yMax = 300
+trumpet5.yMin = 365
+
+trumpet6 = colaider()
+trumpet6.xMin = 5705
+trumpet6.xMax = 5785
+trumpet6.yMax = 300
+trumpet6.yMin = 365
+
+block1 = colaider()
+block1.xMin = 485
+block1.xMax = 545
+block1.yMax = 235
+block1.yMin = 305
+# 
+block2 = colaider()
+block2.xMin = 613
+block2.xMax = 800
+block2.yMax = 235
+block2.yMin = 305
+
+block3 = colaider()
+block3.xMin = 675
+block3.xMax = 735
+block3.yMax = 105
+block3.yMin = 180
+
+block4 = colaider()
+block4.xMin = 2020
+block4.xMax = 2080
+block4.yMax = 205
+block4.yMin = 270
+
+block5 = colaider()
+block5.xMin = 2435
+block5.xMax = 2560
+block5.yMax = 235
+block5.yMin = 305
+
+block6 = colaider()
+block6.xMin = 2531
+block6.xMax = 2815
+block6.yMax = 107
+block6.yMin = 180
+
+block7 = colaider()
+block7.xMin = 2885
+block7.xMax = 3040
+block7.yMax = 107
+block7.yMin = 180
+
+block8 = colaider()
+block8.xMin = 2980
+block8.xMax = 3040
+block8.yMax = 235
+block8.yMin = 305
+
+block9 = colaider()
+block9.xMin = 3175
+block9.xMax = 3260
+block9.yMax = 235
+block9.yMin = 305
+
+block10 = colaider()
+block10.xMin = 3365
+block10.xMax = 3420
+block10.yMax = 235
+block10.yMin = 305
+
+block11 = colaider()
+block11.xMin = 3460
+block11.xMax = 3520
+block11.yMax = 235
+block11.yMin = 305
+
+block12 = colaider()
+block12.xMin = 3555
+block12.xMax = 3615
+block12.yMax = 235
+block12.yMin = 305
+
+block13 = colaider()
+block13.xMin = 3460
+block13.xMax = 3520
+block13.yMax = 107
+block13.yMin = 180
+
+block14 = colaider()
+block14.xMin = 3750
+block14.xMax = 3810
+block14.yMax = 235
+block14.yMin = 305
+
+block15 = colaider()
+block15.xMin = 3845
+block15.xMax = 3965
+block15.yMax = 107
+block15.yMin = 180
+
+block16 = colaider()
+block16.xMin = 4070
+block16.xMax = 4225
+block16.yMax = 107
+block16.yMin = 180
+
+block17 = colaider()
+block17.xMin = 4100
+block17.xMax = 4190
+block17.yMax = 235
+block17.yMin = 305
+
+# лесница {
+block18 = colaider()
+block18.xMin = 4260
+block18.xMax = 4415
+block18.yMax = 330
+block18.yMin = 365
+
+block19 = colaider()
+block19.xMin = 4290
+block19.xMax = 4415
+block19.yMax = 300
+block19.yMin = 365
+
+block20 = colaider()
+block20.xMin = 4320
+block20.xMax = 4415
+block20.yMax = 270
+block20.yMin = 365
+
+block21 = colaider()
+block21.xMin = 4350
+block21.xMax = 4415
+block21.yMax = 236
+block21.yMin = 365
+# }
+
+# loesneza1 {
+block22 = colaider()
+block22.xMin = 4450
+block22.xMax = 4610
+block22.yMax = 330
+block22.yMin = 365
+
+block23 = colaider()
+block23.xMin = 4450
+block23.xMax = 4575
+block23.yMax = 300
+block23.yMin = 365
+
+block24 = colaider()
+block24.xMin = 4450
+block24.xMax = 4545
+block24.yMax = 255
+block24.yMin = 365
+
+block25 = colaider()
+block25.xMin = 4450
+block25.xMax = 4510
+block25.yMax = 240
+block25.yMin = 365
+# }
+
+# loesneza2 {
+block26 = colaider()
+block26.xMin = 4705
+block26.xMax = 4895
+block26.yMax = 330
+block26.yMin = 365
+
+block27 = colaider()
+block27.xMin = 4740
+block27.xMax = 4895
+block27.yMax = 300
+block27.yMin = 365
+
+block28 = colaider()
+block28.xMin = 4770
+block28.xMax = 4895
+block28.yMax = 255
+block28.yMin = 365
+
+block29 = colaider()
+block29.xMin = 4800
+block29.xMax = 4895
+block29.yMax = 240
+block29.yMin = 365
+# }
+
+# loesneza3 {
+block30 = colaider()
+block30.xMin = 4930
+block30.xMax = 5090
+block30.yMax = 330
+block30.yMin = 365
+
+block31 = colaider()
+block31.xMin = 4930
+block31.xMax = 5060
+block31.yMax = 300
+block31.yMin = 365
+
+block32 = colaider()
+block32.xMin = 4930
+block32.xMax = 5025
+block32.yMax = 255
+block32.yMin = 365
+
+block33 = colaider()
+block33.xMin = 4930
+block33.xMax = 4995
+block33.yMax = 240
+block33.yMin = 365
+# }
+
+block34 = colaider()
+block34.xMin = 5350
+block34.xMax = 5505
+block34.yMax = 240
+block34.yMin = 305
+
+# loesneza3 {
+#1
+block35 = colaider()
+block35.xMin = 5785
+block35.xMax = 6080
+block35.yMax = 320
+block35.yMin = 365
+#2
+block36 = colaider()
+block36.xMin = 5795
+block36.xMax = 6080
+block36.yMax = 290
+block36.yMin = 365
+#3
+block37 = colaider()
+block37.xMin = 5825
+block37.xMax = 6080
+block37.yMax = 260
+block37.yMin = 365
+#4
+block38 = colaider()
+block38.xMin = 5860
+block38.xMax = 6080
+block38.yMax = 230
+block38.yMin = 365
+#5
+block39 = colaider()
+block39.xMin = 5895
+block39.xMax = 6080
+block39.yMax = 200
+block39.yMin = 365
+#6
+block40 = colaider()
+block40.xMin = 5925
+block40.xMax = 6080
+block40.yMax = 170
+block40.yMin = 365
+#7
+block41 = colaider()
+block41.xMin = 5960
+block41.xMax = 6080
+block41.yMax = 140
+block41.yMin = 365
+#8
+block42 = colaider()
+block42.xMin = 5985
+block42.xMax = 6080
+block42.yMax = 110
+block42.yMin = 365
+# }
+
+pit1 = colaider()
+pit1.xMin = 2205
+pit1.xMax = 2245
